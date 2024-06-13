9b83397fe91a2dc2eb6e9093497e207d13982778
Daniel Andrei Muresan <daniel.muresan@inetum.com>
2024-06-13 11:47:11 +0200

    Uploaded sample project

---
+++ CollectionViewMemoryLeaks/CollectionViewMemoryLeaks/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using CollectionViewMemoryLeaks.ViewModels;
+
+namespace CollectionViewMemoryLeaks.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}
